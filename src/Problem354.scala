9e58cf7858ee1665573965aaefb05fac3978ef0d
Chris <Chris@localhost>
2011-11-02 20:30:30 +0800

    refactor

--- src/Problem354.scala
+++ src/Problem354.scala
@@ -11,44 +11,57 @@ object Problem354 {
 		var i = 1
 		var y = 0
 		do {
-			y += 1
-			//y = i*i
-			//if (y > Int.MaxValue/4) throw new Exception("out of range...")
-			//i += 1
-			solutions = List[Pair[Int, Int]]()
-			
-			val maxN:Int = (sqrt(4*y/3)).longValue
-			for {
-				n <- -maxN to maxN		// the range where det >= 0
-				root <- asInt(sqrt(det(n, y).doubleValue))
-				if ((n & 1) == (root & 1))
-			}
-			{
-				//println("n: "+n+", root: "+root)
-				if (root == 0)
-				{
-					solutions = (n/2, n) :: solutions
-				}
-				else
-				{
-					solutions = ((n + root) / 2, n) :: ((n - root) / 2, n) :: solutions
-				}
-			}
-			count = solutions.length
+			y = i * i
+			count = countForY(y)
 			max = if (count > max) {
 				println((System.currentTimeMillis() - start)+"ms "+"\ty: "+y+"\tcount: "+count)
 				count
 			}
 			else max
 			
+			y = 3 * y
+			count = countForY(y)
+			max = if (count > max) {
+				println((System.currentTimeMillis() - start)+"ms "+"\ty: "+y+"\tcount: "+count)
+				count
+			}
+			else max
+			
+			i += 1
 		} while (count != 450)
 		
 		println(y)
-		println(solutions.length)
-		println(solutions)
+		//println(solutions.length)
+		//println(solutions)
 		
 		println("runtime: "+(System.currentTimeMillis() - start)+"ms.")
 	}
+	
+	private def countForY(y:Int): scala.Int = {
+		//solutions = List[Pair[Int, Int]]()
+		val maxN:Int = (sqrt(4 * y.doubleValue / 3)).longValue
+		var count:scala.Int = 0
+		
+		for {
+			n <- -maxN to maxN		// the range where det >= 0
+			root <- asInt(sqrt(det(n, y).doubleValue))
+			if ((n & 1) == (root & 1))
+		} {
+			if (root == 0)
+			{
+				//solutions = (n/2, n) :: solutions
+				count += 1
+			}
+			else
+			{
+				//solutions = ((n + root) / 2, n) :: ((n - root) / 2, n) :: solutions
+				count += 2
+			}
+		}
+		
+		count
+	}
+	
 	/*
 	private def solveForIntegerM (n:Int, y:Int): Option[Pair[Int, Option[Int]]] = {
 		
