f7d7aa6000f16c840f532bd327d66bfe4504685f
Chris <dipucian@users.noreply.github.com>
2011-11-02 10:07:51 +0800

    wrote a slow algorithm for Problem354

--- src/Problem354.scala
+++ src/Problem354.scala
@@ -1,19 +1,55 @@
 import math._
+import math.{BigInt => Int}
 
 object Problem354 {
 	def main(args: Array[String]): Unit = {
+		val start = System.currentTimeMillis()
 		
 		var solutions = List[Pair[Int, Int]]()
-		
-		var y=0;
-		
-		for {
-			n <- 0 to (sqrt(4*y/3)).intValue
+		var max = 0
+		var count = 0
+		var i = 1
+		var y = 0
+		do {
+			y += 1
+			//y = i*i
+			//if (y > Int.MaxValue/4) throw new Exception("out of range...")
+			//i += 1
+			solutions = List[Pair[Int, Int]]()
 			
-		}
+			val maxN:Int = (sqrt(4*y/3)).longValue
+			for {
+				n <- -maxN to maxN		// the range where det >= 0
+				root <- asInt(sqrt(det(n, y).doubleValue))
+				if ((n & 1) == (root & 1))
+			}
+			{
+				//println("n: "+n+", root: "+root)
+				if (root == 0)
+				{
+					solutions = (n/2, n) :: solutions
+				}
+				else
+				{
+					solutions = ((n + root) / 2, n) :: ((n - root) / 2, n) :: solutions
+				}
+			}
+			count = solutions.length
+			max = if (count > max) {
+				println((System.currentTimeMillis() - start)+"ms "+"\ty: "+y+"\tcount: "+count)
+				count
+			}
+			else max
+			
+		} while (count != 450)
 		
+		println(y)
+		println(solutions.length)
+		println(solutions)
+		
+		println("runtime: "+(System.currentTimeMillis() - start)+"ms.")
 	}
-	
+	/*
 	private def solveForIntegerM (n:Int, y:Int): Option[Pair[Int, Option[Int]]] = {
 		
 		val d = det (n, y)
@@ -29,7 +65,7 @@ object Problem354 {
 		
 		None
 	}
-	
+	*/
 	private def modf (d:Double): Pair[Int, Double] = {
 		val intPart = d.intValue
 		(intPart, d - intPart)
