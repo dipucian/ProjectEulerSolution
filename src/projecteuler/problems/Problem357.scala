d7493c0aa2b732d8ebb54acc484306c70c88d793
Chris <dipucian@users.noreply.github.com>
2011-11-11 09:32:15 +0800

    add timer

--- src/projecteuler/problems/Problem357.scala
+++ src/projecteuler/problems/Problem357.scala
@@ -3,24 +3,23 @@ package projecteuler.problems
 object Problem357 {
 
 	def main(args: Array[String]): Unit = {
-		
+
 		val problemStatement = "Find the sum of all positive integers n not exceeding 100 000 000" +
-				"\nsuch that for every divisor d of n, d+n/d is prime."
+			"\nsuch that for every divisor d of n, d+n/d is prime."
 		println(problemStatement)
-		
+
 		import util.prime.PrimeUtil._
-		
-		var sum = 1 + 2 +6L
-		for {
-			n <- 10 until 100000000 by 4
-			if (isPrimeGenerator(n))
+		import util.Timer.time
+
+		time {
+			var sum = 1 + 2 + 6L
+			for {
+				n <- 10 until 100000000 by 4
+				if (isPrimeGenerator(n))
+			} sum += n
+
+			println("Answer: " + sum)
 		}
-		{
-			println(n)
-			sum += n
-		}
-		
-		println("Answer: "+sum)
 	}
 
 }
